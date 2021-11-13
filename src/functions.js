d287ec64df6dcaf167779bc2f9b451ba819b1254
Erencan ERTEM <erencanertem@outlook.com>
2021-11-13 12:33:54 +0300

    .

--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,12 @@
-function addToCart(productName) {
-    console.log("Sepete eklendi! : " + productName);
+function addToCart(quantity, productName = "Elma") {
+    console.log("Sepete eklendi! ürün : " + productName + " adet : " + quantity);
 }
 
 
-addToCart()
+// addToCart()
+addToCart(10)
+// addToCart(15);
+
 
 
 
