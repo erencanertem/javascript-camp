d268441d0d665b052ddf0b4b745c9b0ba7d13ea1
Erencan ERTEM <erencanertem@outlook.com>
2021-11-14 17:12:53 +0300

    changed html script

---
+++ src/components/userComponent.js
@@ -0,0 +1,30 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js";
+import User from "../models/user.js"
+import UserService from "../services/userService.js"
+
+console.log("User Component loaded")
+
+let logger1 = new MongoLogger ()
+let userService = new UserService(logger1);
+
+let user1 = new User(1, "Engin", "Demiroğ", "Ankara");
+let user2 = new User(2, "Erencan", "Ertem", "Istanbul");
+userService.add(user1);
+userService.add(user2);
+
+console.log(userService.list())
+console.log(userService.getById(2));
+
+
+userService.getById(1)
+userService.list()
+
+
+
+// let customer = {id:1, firstName: "Engin"};
+
+// // Prototyping
+
+// customer.lastName = "Demiroğ";
+
+// console.log(customer);
