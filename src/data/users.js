a36d8983b0d797419df6cc17f5aa7c4216b7da66
Erencan ERTEM <erencanertem@outlook.com>
2021-11-22 22:54:33 +0300

    timeout

--- src/data/users.js
+++ src/data/users.js
@@ -1,40 +1,45 @@
 // (id, firstName, lastName, city, age)
 
-export const users = [
-    {
-        id:1,
+export const users = [{
+        id: 1,
         firstName: "Engin",
         lastName: "Demiroğ",
-        city: "Ankara",
-        age:36,
+        age: 36,
         creditCardNumber: "123456",
-        type:"customer"
+        type: "customer"
     },
     {
-        id:2,
+        id: 2,
         firstName: "Mustafa",
         lastName: "Sansar",
         city: "İstanbul",
-        age:21,
+        age: "asdasd21a",
         creditCardNumber: "654321",
-        type:"customer"
+        type: "customer"
     },
     {
-        id:3,
+        id: 3,
         firstName: "Cansu",
         lastName: "Değirmen",
         city: "İstanbul",
-        age:20,
         salary: 8000,
-        type:"employee"
+        type: "employee"
     },
     {
-        id:4,
+        id: 4,
         firstName: "Mücahit",
         lastName: "Kaya",
         city: "Bolu",
-        age:23,
-        salary: 8500,
-        type:"employee"
+        age: 23,
+        type: "employee"
     },
+    {
+        id: 5,
+        firstName: "Eren",
+        lastName: "Arı",
+        city: "Diyarbakır",
+        age: 20,
+        salary: 15000,
+        type: "employe"
+    }
 ]
